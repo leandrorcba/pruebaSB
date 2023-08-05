c42dd302f845b7945c71865c45e2b2dd10393c02
lrebolini <lrebolini@makingsense.com>
2023-08-05 05:29:46 -0300

    Adding App

---
+++ src/main/java/ar/com/lbr/pruebaSB/PruebaSbApplication.java
@@ -0,0 +1,13 @@
+package ar.com.lbr.pruebaSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/lbr/pruebaSB/PruebaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.lbr.pruebaSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
